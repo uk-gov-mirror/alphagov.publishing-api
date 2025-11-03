898c011defea5e63f8fd5657963f43ed2251c6d0
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2025-11-03 11:21:31 +0000

    Print paths to query / plan instead of strings

--- spec/benchmark_helper.rb
+++ spec/benchmark_helper.rb
@@ -63,18 +63,21 @@ RSpec::Matchers.define :be_fast_query do |threshold: 0.001|
       [["(Failed to EXPLAIN: #{e.message})"]]
     end
 
+    test_name = RSpec.current_example.description.parameterize
+    fingerprint = Digest::MD5.hexdigest(sql)
+    sql_filepath = "tmp/sql_#{test_name}_#{fingerprint}.sql"
+    File.write(Rails.root.join(sql_filepath), sql)
+
     pretty_plan = plan.respond_to?(:values) ? plan.values.map(&:first).join("\n") : plan.to_s
+
+    query_plan_filepath = "tmp/sql_#{test_name}_#{fingerprint}.plan"
+    File.write(Rails.root.join(query_plan_filepath), pretty_plan)
 
     <<~MSG
       Expected SQL query to complete in under #{@threshold} seconds, but took #{query[:time]}s.
 
-      Query:
-
-      #{sql}
-
-      Query plan:
-
-      #{pretty_plan}
+      SQL query is in: #{sql_filepath}
+      Query plan is in: #{query_plan_filepath}
     MSG
   end
 end
